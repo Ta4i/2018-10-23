b91e9b9e51d899fc772825c911a11b560d4a8bea
Maksym Kramarenko <maksym@wix.com>
2018-10-26 21:41:24 +0300

    Add react-select

--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react'
 import ArticleList from './article-list'
 import articles from '../fixtures'
 import UserForm from './user-form'
+import Select from 'react-select'
 
 export default class App extends Component {
+  state = {
+    selectedOption: null
+  }
   render() {
     return (
       <div>
         <UserForm />
+        <Select
+          options={this.optionsForSelect}
+          onChange={this.handleSelectChange}
+          value={this.state.selectedOption}
+        />
         <ArticleList items={articles} />
       </div>
     )
+  }
+
+  get optionsForSelect() {
+    return articles.map((item) => ({
+      value: item.id,
+      label: item.title
+    }))
+  }
+
+  handleSelectChange = (selectedOption) => {
+    this.setState({ selectedOption })
   }
 }
