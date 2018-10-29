fc5d455c79552f79bda8555efc8ed881e7e8191d
MariaPaschevska <maria.paschevska@gmail.com>
2018-10-29 14:20:17 +0200

    range of dates implemented

--- src/components/day-picker.js
+++ src/components/day-picker.js
@@ -1,30 +1,79 @@
 import React, { Component } from 'react'
-import DayPicker from 'react-day-picker'
+import DayPicker, { DateUtils } from 'react-day-picker'
+import Helmet from 'react-helmet'
 import 'react-day-picker/lib/style.css'
 
 export default class MyCalendar extends Component {
   constructor(props) {
     super(props)
     this.handleDayClick = this.handleDayClick.bind(this)
-    this.state = {
-      selectedDay: undefined
+    this.handleResetClick = this.handleResetClick.bind(this)
+    this.state = this.getInitialState()
+  }
+
+  getInitialState() {
+    return {
+      from: undefined,
+      to: undefined
     }
   }
+
   handleDayClick(day) {
-    this.setState({ selectedDay: day })
+    const range = DateUtils.addDayToRange(day, this.state)
+    this.setState(range)
   }
+
+  handleResetClick() {
+    this.setState(this.getInitialState())
+  }
+
   render() {
+    const { from, to } = this.state
+    const modifiers = { start: from, end: to }
     return (
       <div>
         <DayPicker
+          className="Selectable"
+          selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
           onDayClick={this.handleDayClick}
-          selectedDays={this.state.selectedDay}
         />
-        {this.state.selectedDay ? (
-          <p>You clicked {this.state.selectedDay.toLocaleDateString()}</p>
-        ) : (
-          <p>Please select a day.</p>
-        )}
+        <p>
+          {!from && !to && 'Please select the first day.'}
+          {from && !to && 'Please select the last day.'}
+          {from &&
+            to &&
+            `Selected from ${from.toLocaleDateString()} to
+                ${to.toLocaleDateString()}`}{' '}
+          {from &&
+            to && (
+              <button className="link" onClick={this.handleResetClick}>
+                Reset
+              </button>
+            )}
+        </p>
+
+        <Helmet>
+          <style>
+            {`
+            .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+              background-color: #f0f8ff !important;
+              color: #4a90e2;
+            }
+            .Selectable .DayPicker-Day {
+              border-radius: 0 !important;
+            }
+            .Selectable .DayPicker-Day--start {
+              border-top-left-radius: 50% !important;
+              border-bottom-left-radius: 50% !important;
+            }
+            .Selectable .DayPicker-Day--end {
+              border-top-right-radius: 50% !important;
+              border-bottom-right-radius: 50% !important;
+            }
+          `}
+          </style>
+        </Helmet>
       </div>
     )
   }
