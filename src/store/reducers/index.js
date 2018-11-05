dcec1a1ece0d75a43764a0cfaba71f0793b656a7
Troayn Victor <troyanvic@gmail.com>
2018-11-05 16:00:41 +0200

    training redux flow on UserForm Component

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import counterReducer from './counter'
 import articlesReducer from './articles'
+import usernameReducer from './userForm'
 
 export default combineReducers({
   count: counterReducer,
-  articles: articlesReducer
+  articles: articlesReducer,
+  username: usernameReducer
 })
