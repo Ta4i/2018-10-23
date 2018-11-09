140ea7b2252006436f26c87249feca83c711bd27
MariaPaschevska <maria.paschevska@gmail.com>
2018-11-09 17:22:29 +0200

    Articles are now stored as key-value

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,10 +1,17 @@
 import { DELETE_ARTICLE } from '../constants/index'
 import { normalizedArticles } from '../fixtures'
 
-export default (articleState = normalizedArticles, action) => {
+const defaultArticle = normalizedArticles.reduce((acc, article) => {
+  acc[article.id] = article
+  return acc
+}, {})
+
+export default (articleState = defaultArticle, action) => {
   if (action.type === DELETE_ARTICLE) {
     console.log('Article Reducer - articleState', articleState)
-    return articleState.filter((article) => article.id !== action.payload.id)
+    return Object.values(articleState).filter(
+      (article) => article.id !== action.payload.id
+    )
   }
   return articleState
 }
