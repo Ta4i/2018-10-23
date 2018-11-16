add1dc1f8e594f28b9e698dfd1d01d32af532601
Maksym Kramarenko <maksym@wix.com>
2018-11-16 19:09:59 +0200

    Add routing

--- src/components/app.js
+++ src/components/app.js
@@ -3,15 +3,33 @@ import ArticleList from './article-list'
 import UserForm from './user-form'
 import Filters from './filters'
 import Counter from './counter'
+import { Route, NavLink } from 'react-router-dom'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <UserForm />
-        <Counter />
-        <Filters articles={[]} />
-        <ArticleList />
+        <div>
+          <div>
+            <NavLink to="/counter" activeClassName="active-menu">
+              Counter
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/filters" activeStyle={{ color: 'red' }}>
+              Filters
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/articles" activeStyle={{ color: 'red' }}>
+              Articles
+            </NavLink>
+          </div>
+        </div>
+        <Route path="/counter" component={Counter} />
+        <Route path="/filters" component={Filters} />
+        <Route path="/articles" component={ArticleList} />
       </div>
     )
   }
